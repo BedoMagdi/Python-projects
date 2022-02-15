5494c6763c4078ac9515d5655f68d7c20e5fcf0c
Abdelrahman Baz <bedomagdy10@gmail.com>
2022-02-14 20:14:30 -0600

    we have a basic ship that fires bullets.

--- alien-invasion/alien_invasion.py
+++ alien-invasion/alien_invasion.py
@@ -1,26 +1,28 @@
-import sys
 import pygame
+from pygame.sprite import Group
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initialize game and create a screen object.
     pygame.init()
     settings = Settings()
-    screen = pygame.display.set_mode(settings.screen_width, settings.screen_height)
+    screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
 
     pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
 
     # Start the main loop for the game.
     while True:
         # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        #Redraw the screen during each pass through the loop.
-        screen.fill(settings.bg_color)
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events(settings, screen, ship, bullets)
+        ship.update()
+        bullets.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(settings, screen, ship, bullets)
         
 run_game()
--- alien-invasion/ship.py
+++ alien-invasion/ship.py
@@ -4,36 +4,33 @@ import pygame
 class Ship:
     """A class to manage the ship."""
  
-    def __init__(self, ai_game):
+    def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position."""
-        self.screen = ai_game.screen
-        self.settings = ai_game.settings
-        self.screen_rect = ai_game.screen.get_rect()
-
-        # Load the ship image and get its rect.
+        self.screen = screen
+        self.ai_settings = ai_settings
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
 
-        # Start each new ship at the bottom center of the screen.
-        self.rect.midbottom = self.screen_rect.midbottom
+        # Start each new ship at the bottom
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
 
-        # Store a decimal value for the ship's horizontal position.
-        self.x = float(self.rect.x)
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
 
-        # Movement flags
+        # Movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        """Update the ship's position based on movement flags."""
-        # Update the ship's x value, not the rect.
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.x += self.settings.ship_speed
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
-            self.x -= self.settings.ship_speed
+            self.center -= self.ai_settings.ship_speed_factor
 
-        # Update rect object from self.x.
-        self.rect.x = self.x
+        # Update rect object from self.center.
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Draw the ship at its current location."""
