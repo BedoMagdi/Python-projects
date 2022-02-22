e6c983465381fc3361363d16631c8d2cd74a5106
Abdelrahman Baz <bedomagdy10@gmail.com>
2022-02-21 21:33:11 -0600

    added stats and end game

--- alien-invasion/alien_invasion.py
+++ alien-invasion/alien_invasion.py
@@ -4,6 +4,7 @@ from settings import Settings
 from ship import Ship
 from alien import Alien
 import game_functions as gf
+from game_stats import GameStats
 
 def run_game():
     # Initialize game and create a screen object.
@@ -12,7 +13,7 @@ def run_game():
     screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
 
     pygame.display.set_caption("Alien Invasion")
-
+    stats = GameStats(settings)
     ship = Ship(settings, screen)
     # Make a group to store bullets in.
     bullets = Group()
@@ -24,10 +25,11 @@ def run_game():
     while True:
         # Watch for keyboard and mouse events.
         gf.check_events(settings, screen, ship, bullets)
-        ship.update()
-        bullets.update()
-        gf.update_bullets(bullets)
-        gf.update_aliens(settings, aliens)
+        if stats.game_active:
+            ship.update()
+            bullets.update()
+            gf.update_bullets(settings, screen, ship, aliens, bullets)
+            gf.update_aliens(settings, stats, screen, ship, aliens, bullets)
         gf.update_screen(settings, screen, ship, aliens, bullets)
         
 run_game()
