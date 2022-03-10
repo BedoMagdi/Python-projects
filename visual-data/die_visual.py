1b025401ad4aa6b12c3a881bec2cccdfeea936a1
Abdelrahman Baz <bedomagdy10@gmail.com>
2022-03-09 21:05:52 -0600

    visualize temps

--- visual-data/die_visual.py
+++ visual-data/die_visual.py
@@ -2,8 +2,8 @@ import pygal
 from die import Die
 
 # Create two D6 dice.
-die_1 = Die()
-die_2 = Die()
+die_1 = Die(8)
+die_2 = Die(8)
 
 # Make some rolls, and store results in a list.
 results = []
@@ -23,9 +23,9 @@ for value in range(2, max_result+1):
 # Visualize the results.
 hist = pygal.Bar()
 
-hist.title = "Results of rolling two D6 dice 1000 times."
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.title = "Results of rolling a D8 and a D8 1000 times."
+hist.x_labels = [i for i in range(2, die_1.num_sides + die_2.num_sides + 1)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
-hist.add('D6 + D6', frequencies)
+hist.add('D8 + D18', frequencies)
 hist.render_to_file('die_visual.svg')
